b88095a2eedfacc2eaf181d3a62af14c00b4b51d
Jeffery R Scott <jscott@svante.(none)>
2011-02-18 17:04:28 -0500

    add missing ocean carbon file

---
+++ inc/OCM.h
@@ -0,0 +1,7 @@
+      COMMON/OCARBUPT/ocupt,co24ocnan !jrs,ncallatm
+       REAL*4 ocupt
+       REAL*4 co24ocnan(jm0)
+Cjrs       REAL*4 ncallatm
+
+      COMMON/ANNTCU/temuptann
+       REAL*4 temuptann
