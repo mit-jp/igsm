f99d1cb6147f96ea9854c372edf071ddd70359aa
Jeffery R Scott <jscott@svante.(none)>
2011-06-03 17:24:11 -0400

    additional cleanup of driver variables...

--- inc/OCM.h
+++ inc/OCM.h
@@ -1,7 +1,11 @@
       COMMON/OCARBUPT/ocupt,co24ocnan !jrs,ncallatm
        REAL*4 ocupt
        REAL*4 co24ocnan(jm0)
-Cjrs       REAL*4 ncallatm
+
+      COMMON /DRIVER_VAR_CO2/
+     &         ocumn, fluxco2mn
+       REAL*8 ocumn
+       REAL*8 fluxco2mn(jm0)
 
       COMMON/ANNTCU/temuptann
        REAL*4 temuptann
