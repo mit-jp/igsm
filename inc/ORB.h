4ceee6fd2a862f8655660f1cfc58cbacfa463b6d
Jeffery R Scott <jscott@svante.(none)>
2011-06-03 16:36:55 -0400

    changes in includes (adding ORB.h) for AR5 + aviation mods

---
+++ inc/ORB.h
@@ -0,0 +1,4 @@
+      parameter (nyorbit=2101,ncomments=26)
+!     should be change if different data file is used
+      common/ORBPARAM/ yorbit(nyorbit),eccenh(nyorbit),obliqh(nyorbit),
+     &  perih(nyorbit)
